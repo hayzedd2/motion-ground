9e5e7095850c2f37ade82c6dc53c76c3e8de2ac8
hayzedd2 <azeezalhameen1@gmail.com>
2024-09-03 18:09:57 +0100

    code refactor

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,15 @@ import Toolbar from "./components/Toolbar";
 import Filter from "./components/Filter";
 import Drawer from "./components/Drawer";
 import Offset from "./components/Offset";
+import Pair from "./components/Pair";
 
 const playgroundPage = () => {
   return (
     <section className="bg-[rgb(17,17,16)]">
       <main className="text-[#d1d1cb] max-w-[40rem] mx-auto pb-20">
         <AboutMe />
-        <Offset/>
+        {/* <Offset/> */}
+        {/* <Pair/> */}
         <Drawer />
         <Filter />
         <Toolbar />
