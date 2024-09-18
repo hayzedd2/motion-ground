dda093f2423cf8f0901e95fceff9a0a42de5f889
hayzedd2 <azeezalhameen1@gmail.com>
2024-09-18 11:00:57 +0100

    components rearrangement

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,9 @@ const playgroundPage = () => {
   return (
     <section className="bg-[rgb(17,17,16)]">
       <main className="text-[#d1d1cb] max-w-[40rem] mx-auto pb-20">
-        <SubtleInteraction/>
+        
         <AboutMe />
+        <SubtleInteraction/>
         <PortfolioWidget/>
         <VoiceChat />
         <Pair />
