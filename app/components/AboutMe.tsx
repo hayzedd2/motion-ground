0ff7f3f01b5d88a150ce0911b56faa92e1f5d706
hayzedd2 <azeezalhameen1@gmail.com>
2025-03-31 08:31:23 +0100

    rm readme + fixed portfolio link

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -5,7 +5,7 @@ export const AboutMe = () => {
         <div className="px-4 pt-20 flex flex-col gap-1 opacity-80 text-[1.1rem]">
           <p>Hi there, i am <span className="underline  underline-offset-2">Alhameen</span>, a frontend engineer based in Lagos.</p>
           <p>This is my playground, i just like to experiment creating some interactions i see online (currently learning framer-motion)</p>
-          <a href="https://alhameen.xyz" target="_blank" className="w-full  flex items-end justify-end underline underline-offset-2">See my portfolio</a>
+          <a href="https://www.alhameen.xyz/" target="_blank" className="w-full  flex items-end justify-end underline underline-offset-2">See my portfolio</a>
         </div>
        
       </>
